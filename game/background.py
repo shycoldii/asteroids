113a2ecefc519f5497c9415e96bbce70ed2933f5
Alexey Kovalev <41exey.kov41ev@gmail.com>
2020-12-11 11:31:01 +0300

    resize

--- game/background.py
+++ game/background.py
@@ -9,16 +9,19 @@ class Background:
 
     def __init__(self, display):
         self._display = display
+        self._size = self._display.get_size
         self.image = pg.transform.scale(pg.image.load("./data/background.jpg").convert(), self._display.get_size())
         self.rect = self.image.get_rect(center=(self._display.get_width() / 2, self._display.get_height() / 2))
         self._asteroids = [Asteroid(display=self._display) for _ in range(22)]
 
     def update(self):
+        if self._size != self._display.get_size():
+            self.image = pg.transform.scale(self.image, self._display.get_size())
+            self._size = self._display.get_size
         for asteroid in self._asteroids:
             asteroid.update()
 
     def draw(self, surface=None):
-        print(self._display.size)
         if surface is not None:
             surface.blit(self.image, self.rect)
         elif self._display is not None:
